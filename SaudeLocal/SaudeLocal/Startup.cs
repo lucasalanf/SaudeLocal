50bce9c6030fc3ee79371b8dd4f524e514dec2fd
EGFernandes <eliseo.fernandes@gmail.com>
2017-02-08 14:38:08 -0200

    Create project

---
+++ SaudeLocal/SaudeLocal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaudeLocal.Startup))]
+namespace SaudeLocal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
